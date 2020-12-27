620781579ad10643ff0b44c746e87e1d777e968b
george_kpb <georgiy.kuznetsov.19@mail.ru>
2020-12-27 23:54:33 +0600

    Рефакторинг кода. State и сопряженные функции в Store.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import state, {addPost, changeNewPostText, subscribe} from './redux/state'
+import store from './redux/state'
 import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -10,14 +10,14 @@ let renderDOMTree = () => {
   ReactDOM.render(
     <BrowserRouter>
       <React.StrictMode>
-        <App data={state} addPost={addPost} changeNewPostText={changeNewPostText}/>
+        <App data={store.getState()} addPost={store.addPost.bind(store)} changeNewPostText={store.changeNewPostText.bind(store)}/>
       </React.StrictMode>
     </BrowserRouter>,
     document.getElementById('root')
   );
 };
-renderDOMTree()
-subscribe(renderDOMTree);
+renderDOMTree();
+store.subscribe(renderDOMTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,102 +1,132 @@
 
 
-const state = {
-  user: {
-    login: 'george_kpb',
-    photo: 'https://i03.fotocdn.net/s114/2f5ff212bcc7d615/user_l/2577837327.jpg',
-    id: '54562123123155645',
-    name: 'Георгий',
-    surname: 'Кузнецов',
-    dateOfBirth: '13.04.1997',
-    profileCover: 'https://vk-oblozhki.ru/photos/big/background-color-explosion-sky-abstact-colors-125-211.jpg',
-    location: 'Омск',
+const store = {
+  _state: {
+    user: {
+      login: 'george_kpb',
+      photo: 'https://i03.fotocdn.net/s114/2f5ff212bcc7d615/user_l/2577837327.jpg',
+      id: '54562123123155645',
+      name: 'Георгий',
+      surname: 'Кузнецов',
+      dateOfBirth: '13.04.1997',
+      profileCover: 'https://vk-oblozhki.ru/photos/big/background-color-explosion-sky-abstact-colors-125-211.jpg',
+      location: 'Омск',
+    },
+
+    postPage: {
+      posts: [
+        { id: '564641231', 
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ducimus porro tempore. Cumque, quasi aliquid pariatur rem delectus dolorem, quisquam possimus exercitationem blanditiis, eveniet optio. Similique dolores perspiciatis veniam velit.',
+          date: '19.12.2020',
+        },
+        { id: '564641231', 
+          text: 'Мой первый пост',
+          date: '17.12.2020',
+        },
+        { id: '564641231', 
+          text: 'Мой первый пост',
+          date: '17.12.2020',
+        },
+        { id: '345235345', 
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ducimus porro tempore. Cumque, quasi aliquid pariatur rem delectus dolorem, quisquam possimus exercitationem blanditiis, eveniet optio. Similique dolores perspiciatis veniam velit.',
+          date: '14.12.2020',
+        },
+        { id: '456745567', 
+          text: 'Мой ТРетий пост',
+          date: '13.12.2020',
+        },
+      ],
+      newPostText: '',
+    },
+
+    dialogs: [{ id: 'mark123456', name: 'Марк', messages: [
+                                                              { id: '123', message: '1 сообщение Марк', time: '10:50', date: '20.12.2020'},
+                                                              { id: '456', message: '2 сообщение', time: '10:50', date: '20.12.2020'},
+                                                              { id: '789', message: '3 сообщение', time: '10:50', date: '20.12.2020'},
+                                                              { id: '987', message: '4 сообщение', time: '10:50', date: '20.12.2020'}
+                                                            ],
+                },
+                { id: 'valentin12456', name: 'Валентин', messages: [
+                                                              { id: '123', message: '1 сообщение Валентин', time: '10:50', date: '20.12.2020'},
+                                                              { id: '456', message: '2 сообщение', time: '10:50', date: '20.12.2020'},
+                                                              { id: '789', message: '3 сообщение', time: '10:50', date: '20.12.2020'},
+                                                              { id: '987', message: '4 сообщение', time: '10:50', date: '20.12.2020'}
+                                                            ],
+                },
+                { id: 'pavel12456', name: 'Павел', messages: [
+                                                              { id: '123', message: '1 сообщение Павел', time: '10:50', date: '20.12.2020'},
+                                                              { id: '456', message: '2 сообщение', time: '10:50', date: '20.12.2020'},
+                                                              { id: '789', message: '3 сообщение', time: '10:50', date: '20.12.2020'},
+                                                              { id: '987', message: '4 сообщение', time: '10:50', date: '20.12.2020'}
+                                                            ],
+                },
+                { id: 'Andrey123456', name: 'Андрей', messages: [
+                                                              { id: '123', message: '1 сообщение Андрей', time: '10:50', date: '20.12.2020' },
+                                                              { id: '456', message: '2 сообщение Андрей', time: '10:50', date: '20.12.2020' },
+                                                              { id: '789', message: '3 сообщение', time: '10:50', date: '20.12.2020' },
+                                                              { id: '987', message: '4 сообщение', time: '10:50', date: '20.12.2020' }
+                                                            ],
+                },],
   },
-
-  postPage: {
-    posts: [
-      { id: '564641231', 
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ducimus porro tempore. Cumque, quasi aliquid pariatur rem delectus dolorem, quisquam possimus exercitationem blanditiis, eveniet optio. Similique dolores perspiciatis veniam velit.',
-        date: '19.12.2020',
-      },
-      { id: '564641231', 
-        text: 'Мой первый пост',
-        date: '17.12.2020',
-      },
-      { id: '564641231', 
-        text: 'Мой первый пост',
-        date: '17.12.2020',
-      },
-      { id: '345235345', 
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ducimus porro tempore. Cumque, quasi aliquid pariatur rem delectus dolorem, quisquam possimus exercitationem blanditiis, eveniet optio. Similique dolores perspiciatis veniam velit.',
-        date: '14.12.2020',
-      },
-      { id: '456745567', 
-        text: 'Мой ТРетий пост',
-        date: '13.12.2020',
-      },
-    ],
-    newPostText: '',
+  getState() {
+    return this._state;
   },
+  observerState(state) {
 
-  dialogs: [{ id: 'mark123456', name: 'Марк', messages: [
-                                                            { id: '123', message: '1 сообщение Марк', time: '10:50', date: '20.12.2020'},
-                                                            { id: '456', message: '2 сообщение', time: '10:50', date: '20.12.2020'},
-                                                            { id: '789', message: '3 сообщение', time: '10:50', date: '20.12.2020'},
-                                                            { id: '987', message: '4 сообщение', time: '10:50', date: '20.12.2020'}
-                                                          ],
-              },
-              { id: 'valentin12456', name: 'Валентин', messages: [
-                                                            { id: '123', message: '1 сообщение Валентин', time: '10:50', date: '20.12.2020'},
-                                                            { id: '456', message: '2 сообщение', time: '10:50', date: '20.12.2020'},
-                                                            { id: '789', message: '3 сообщение', time: '10:50', date: '20.12.2020'},
-                                                            { id: '987', message: '4 сообщение', time: '10:50', date: '20.12.2020'}
-                                                          ],
-              },
-              { id: 'pavel12456', name: 'Павел', messages: [
-                                                            { id: '123', message: '1 сообщение Павел', time: '10:50', date: '20.12.2020'},
-                                                            { id: '456', message: '2 сообщение', time: '10:50', date: '20.12.2020'},
-                                                            { id: '789', message: '3 сообщение', time: '10:50', date: '20.12.2020'},
-                                                            { id: '987', message: '4 сообщение', time: '10:50', date: '20.12.2020'}
-                                                          ],
-              },
-              { id: 'Andrey123456', name: 'Андрей', messages: [
-                                                            { id: '123', message: '1 сообщение Андрей', time: '10:50', date: '20.12.2020' },
-                                                            { id: '456', message: '2 сообщение Андрей', time: '10:50', date: '20.12.2020' },
-                                                            { id: '789', message: '3 сообщение', time: '10:50', date: '20.12.2020' },
-                                                            { id: '987', message: '4 сообщение', time: '10:50', date: '20.12.2020' }
-                                                          ],
-              },],
+  },
+  subscribe(observer) {
+    this.observerState = observer;
+  },
+  addPost() {
+    if (this._state.postPage.newPostText === '') return;
+    let newPost = {
+      id: this.getNewId(),
+      text: this._state.postPage.newPostText,
+      date: '19.12.2020',
+    };
+    this._state.postPage.posts.unshift(newPost)
+    this.observerState(this._state);
+  },
+  changeNewPostText(newText) {
+    this._state.postPage.newPostText = newText;
+    this.observerState(this._state);
+  },
+  getNewId(min = 0, max = 10000) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor( Math.random() * (max - min + 1) ) + min;
+  },
 };
-export default state;
+export default store;
 
-let observerState = (state) => {
+// let observerState = (state) => {
 
-}
+// }
 
-export  let subscribe = (observer) => {
-  observerState = observer;
-};
+// export  let subscribe = (observer) => {
+//   observerState = observer;
+// };
 
-export let addPost = () => { 
-  if (state.postPage.newPostText === '') return;
-  let newPost = {
-    id: getNewId(),
-    text: state.postPage.newPostText,
-    date: '19.12.2020',
-  };
-  state.postPage.posts.unshift(newPost)
-  observerState(state);//renderDOMTree(state);
-};
+// export let addPost = () => { 
+//   if (state.postPage.newPostText === '') return;
+//   let newPost = {
+//     id: getNewId(),
+//     text: state.postPage.newPostText,
+//     date: '19.12.2020',
+//   };
+//   state.postPage.posts.unshift(newPost)
+//   observerState(state);//renderDOMTree(state);
+// };
 
-export const changeNewPostText = newText => {
-  state.postPage.newPostText = newText;
-  observerState(state);//renderDOMTree(state);
-}; 
+// export const changeNewPostText = newText => {
+//   state.postPage.newPostText = newText;
+//   observerState(state);//renderDOMTree(state);
+// }; 
 
-function getNewId(min = 0, max = 10000) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor( Math.random() * (max - min + 1) ) + min;
-};
+// function getNewId(min = 0, max = 10000) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor( Math.random() * (max - min + 1) ) + min;
+// };
 
 
